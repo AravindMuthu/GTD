fc95e5d29d7ea849f7c2c5d2f3cdc17569414113
Aravind <aravind.geek@gmail.com>
2009-12-10 16:10:27 +0530

    Added urls to access login and signup etc

--- urls.py
+++ urls.py
@@ -1,15 +1,24 @@
 from django.conf.urls.defaults import *
-from Todo.views import entry_todo,show_todo,todoform,entry_delete
+from Todo.views import entry_todo,show_todo,todoform,entry_delete,login,create_user,signup,logout,show,update,delete_item
+
 from django.conf import settings
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
+                       ('^GTD/$',login),
+                       ('^login/$',login),
+                       ('^signup/$',signup),
+                       ('^create_user/$',create_user),
                        ('^todo/$',todoform),
                        ('^entrytodb/$',entry_todo),
                        ('^getfromdb/$',show_todo),
                        ('^delete/$',entry_delete),
+                       ('^logout/$',logout),
+                       ('^show/(?P<id>\d+)/$',show),
+                       ('^update/(?P<id>\d+)/$',update),
+                       ('^delete_item/(?P<id>\d+)/$',delete_item),
     # Example:
     # (r'^DjangoTodo/', include('DjangoTodo.foo.urls')),
 
@@ -18,10 +27,14 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+                       (r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
     urlpatterns +=patterns('',
                            ('^static/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.MEDIA_ROOT}),
+                         
 )
+
+
+
